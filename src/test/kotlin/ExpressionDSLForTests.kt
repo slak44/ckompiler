ae8a1ffd9337841fa8cbab0657bec70fce7b11ef
Stefan Silviu <sylviu44@gmail.com>
2018-11-04 11:17:20 +0200

    Move expression DSL to separate file

---
+++ src/test/kotlin/ExpressionDSLForTests.kt
@@ -0,0 +1,33 @@
+import java.lang.IllegalArgumentException
+
+internal fun int(i: Long): IntegerConstantNode = IntegerConstantNode(i, IntegralSuffix.NONE)
+
+internal class BinaryBuilder {
+  var lhs: ASTNode? = null
+  var rhs: ASTNode? = null
+  fun build(op: Operators): BinaryNode {
+    return BinaryNode(op, lhs!!, rhs!!)
+  }
+}
+
+internal fun Operators.with(block: BinaryBuilder.() -> Unit): BinaryNode {
+  val b = BinaryBuilder()
+  b.block()
+  return b.build(this)
+}
+
+internal infix fun <LHS, RHS> Pair<LHS, RHS>.with(op: Operators): BinaryNode {
+  if (first is ASTNode && second is ASTNode) {
+    return BinaryNode(op, first as ASTNode, second as ASTNode)
+  }
+  if (first is Int && second is Int) {
+    return BinaryNode(op, int((first as Int).toLong()), int((second as Int).toLong()))
+  }
+  if (first is Int && second is ASTNode) {
+    return BinaryNode(op, int((first as Int).toLong()), second as ASTNode)
+  }
+  if (first is ASTNode && second is Int) {
+    return BinaryNode(op, first as ASTNode, int((second as Int).toLong()))
+  }
+  throw IllegalArgumentException("Bad types")
+}
--- src/test/kotlin/ParserPseudoUnitTests.kt
+++ src/test/kotlin/ParserPseudoUnitTests.kt
@@ -1,5 +1,4 @@
 import org.junit.Test
-import java.lang.IllegalArgumentException
 import kotlin.test.assertEquals
 
 /**
@@ -11,38 +10,6 @@ class ParserPseudoUnitTests {
     val lexer = Lexer(s, source)
     lexer.assertNoDiagnostics()
     return Parser(lexer.tokens, source, s, lexer.tokStartIdxes)
-  }
-
-  private fun int(i: Long): IntegerConstantNode = IntegerConstantNode(i, IntegralSuffix.NONE)
-
-  private class BinaryBuilder {
-    var lhs: ASTNode? = null
-    var rhs: ASTNode? = null
-    fun build(op: Operators): BinaryNode {
-      return BinaryNode(op, lhs!!, rhs!!)
-    }
-  }
-
-  private fun Operators.with(block: BinaryBuilder.() -> Unit): BinaryNode {
-    val b = BinaryBuilder()
-    b.block()
-    return b.build(this)
-  }
-
-  private infix fun <LHS, RHS> Pair<LHS, RHS>.with(op: Operators): BinaryNode {
-    if (first is ASTNode && second is ASTNode) {
-      return BinaryNode(op, first as ASTNode, second as ASTNode)
-    }
-    if (first is Int && second is Int) {
-      return BinaryNode(op, int((first as Int).toLong()), int((second as Int).toLong()))
-    }
-    if (first is Int && second is ASTNode) {
-      return BinaryNode(op, int((first as Int).toLong()), second as ASTNode)
-    }
-    if (first is ASTNode && second is Int) {
-      return BinaryNode(op, first as ASTNode, int((second as Int).toLong()))
-    }
-    throw IllegalArgumentException("Bad types")
   }
 
   @Test
