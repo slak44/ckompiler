7d2135d2a55685fd849bb5a2b65d8f0aca8e103b
Stefan Silviu <sylviu44@gmail.com>
2018-11-15 22:48:13 +0200

    Add more expression parsing tests

--- src/test/kotlin/slak/test/parser/ExpressionTests.kt
+++ src/test/kotlin/slak/test/parser/ExpressionTests.kt
@@ -1,7 +1,10 @@
 package slak.test.parser
 
 import org.junit.Test
+import slak.ckompiler.DiagnosticId
+import slak.ckompiler.ErrorNode
 import slak.test.*
+import kotlin.test.assertEquals
 
 class ExpressionTests {
   @Test
@@ -9,5 +12,44 @@ class ExpressionTests {
     val p = prepareCode("int a = 1 + 2 * 3;", source)
     p.assertNoDiagnostics()
     int declare ("a" assign (1 add (2 mul 3))) assertEquals p.root.getDeclarations()[0]
+  }
+
+  @Test
+  fun exprArithmParensLevel1() {
+    val p = prepareCode("int a = (1 + 2) * 3;", source)
+    p.assertNoDiagnostics()
+    int declare ("a" assign ((1 add 2) mul 3)) assertEquals p.root.getDeclarations()[0]
+  }
+
+  @Test
+  fun exprArithmParensLevel2() {
+    val p = prepareCode("int a = (1 + (2 + 3)) * 3;", source)
+    p.assertNoDiagnostics()
+    int declare ("a" assign ((1 add (2 add 3)) mul 3)) assertEquals p.root.getDeclarations()[0]
+  }
+
+  @Test
+  fun exprArithmParensLevelLots() {
+    val p = prepareCode("int a = (1 + (2 + (3 + (4 + (5 + (6)))))) * 3;", source)
+    p.assertNoDiagnostics()
+    int declare ("a" assign ((1 add (2 add (3 add (4 add (5 add 6))))) mul 3)) assertEquals
+        p.root.getDeclarations()[0]
+  }
+
+  @Test
+  fun exprArithmUnmatchedParens() {
+    val p = prepareCode("int a = (1 * (2 + 3);", source)
+    assertEquals(listOf(
+        DiagnosticId.UNMATCHED_PAREN, DiagnosticId.MATCH_PAREN_TARGET), p.diags.map { it.id })
+    int declare ("a" assign ErrorNode()) assertEquals p.root.getDeclarations()[0]
+  }
+
+  @Test
+  fun exprArithmUnmatchedRightParenAtEndOfExpr() {
+    val p = prepareCode("int a = 1 + 1);", source)
+    assertEquals(listOf(
+        DiagnosticId.EXPECTED_SEMI_AFTER,
+        DiagnosticId.EXPECTED_EXTERNAL_DECL), p.diags.map { it.id })
+    int declare ("a" assign (1 add 1)) assertEquals p.root.getDeclarations()[0]
   }
 }
