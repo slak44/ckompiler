6b96907ade279b335e3cf1e724e077d76d497c77
Stefan Silviu <sylviu44@gmail.com>
2018-11-23 17:32:13 +0200

    Implement CLI, partially

--- src/main/kotlin/slak/ckompiler/Main.kt
+++ src/main/kotlin/slak/ckompiler/Main.kt
@@ -1,7 +1,35 @@
 package slak.ckompiler
 
+import kotlinx.cli.CommandLineInterface
+import kotlinx.cli.HelpPrintedException
+import kotlinx.cli.parse
+import kotlinx.cli.positionalArgumentsList
 import mu.KotlinLogging
+import java.io.File
+import java.lang.Exception
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-  KotlinLogging.logger("Main").info { "Main" }
+  val logger = KotlinLogging.logger("CLI")
+  val cli = CommandLineInterface("ckompiler")
+  val files by cli.positionalArgumentsList(
+      "FILES...", "Translation units to be compiled", minArgs = 1)
+  try {
+    cli.parse(args)
+  } catch (err: Exception) {
+    if (err is HelpPrintedException) exitProcess(1)
+    logger.error(err) { "Failed to parse CLI args" }
+    exitProcess(1)
+  }
+  files.map { File(it) }.forEach {
+    val text = it.readText()
+    val l = Lexer(text, it.absolutePath)
+    if (l.inspections.isNotEmpty()) {
+      return@forEach
+    }
+    val p = Parser(l.tokens, it.absolutePath, text, l.tokStartIdxes)
+    if (p.diags.isNotEmpty()) {
+      return@forEach
+    }
+  }
 }
