b99a888bae70de20d1dc42305497a370046ce0b5
Stefan Silviu <sylviu44@gmail.com>
2018-12-27 12:16:12 +0200

    Work on code generation

--- src/main/kotlin/slak/ckompiler/CodeGenerator.kt
+++ src/main/kotlin/slak/ckompiler/CodeGenerator.kt
@@ -3,45 +3,43 @@ package slak.ckompiler
 import mu.KotlinLogging
 import slak.ckompiler.parser.*
 
+typealias Instructions = List<String>
+
 /**
  * Generate [NASM](https://www.nasm.us/) code from a [RootNode].
  *
- * **NOTE**: This class assumes that the [ast] and all its sub-nodes are valid; it calls
- * [EitherNode.asVal] unconditionally, and performs no correctness checks. For example, a
- * [BinaryExpression] cannot have an [IntegralConstant] lhs and a [FloatingConstant] rhs; the
- * necessary conversion is not performed here.
+ * Warning: the AST is presumed to be valid; instances of [ErrorNode] will likely cause crashes.
  */
 class CodeGenerator(val ast: RootNode) {
-  private val before = mutableListOf<String>()
+  private val prelude = mutableListOf<String>()
   private val text = mutableListOf<String>()
   private val data = mutableListOf<String>()
+
+  private data class Register(val name: String, var itemName: String)
+
+  private val registers = List(6) { Register("r${it + 10}", "") }
 
   companion object {
     private val logger = KotlinLogging.logger("CodeGenerator")
   }
 
   init {
-//    // FIXME: remove filter
-//    ast.decls.map { it.asVal() }
-//        .filter { it is FunctionDefinition }
-//        .forEach { genFunction(it as FunctionDefinition) }
+    ast.decls.forEach {
+      if (it is FunctionDefinition) genFunction(it)
+      else TODO("ext decls")
+    }
   }
 
-//  /**
-//   * Coerces an [EitherNode] to the concrete value of type [N].
-//   * @throws InternalCompilerError if called on an [ErrorNode]
-//   * @return [EitherNode.Value.value]
-//   */
-//  private fun <N : ASTNode> EitherNode<N>.asVal(): N {
-//    if (this is ErrorNode) {
-//      logger.throwICE("An error node was coerced to a real node") { this }
-//    }
-//    return (this as EitherNode.Value).value
-//  }
-
-  private fun List<String>.joinInstructions(): String {
+  private fun Instructions.joinInstructions(): String {
     return joinToString("\n") { "  $it" }
   }
+
+  private val FunctionDefinition.name get() = functionDeclarator.name
+  private val FunctionDefinition.block get() = compoundStatement as CompoundStatement
+
+  private val DeclarationItem.it get() = declaration as RealDeclaration
+
+  private val Declarator.name get() = name()!!.name
 
   private fun genBinExpr(e: BinaryExpression): String {
     val instr = mutableListOf<String>()
@@ -51,39 +49,54 @@ class CodeGenerator(val ast: RootNode) {
     return instr.joinInstructions()
   }
 
-  private fun genStatement(f: FunctionDefinition, it: Statement): String = when (it) {
-    // FIXME: implement this
-    is ReturnStatement -> {
-      if (it.expr == null) {
-        "ret"
-      } else {
-        ""
-      }
-    }
-    else -> TODO()
+  private fun genExpr(e: Expression): Register {
+    // FIXME: implement
+    return Register("r8", "temp")
   }
 
-//  private fun genFunction(f: FunctionDefinition) {
-//    // FIXME: the declarator can also be a pointer+ident combo
-//    val name = (f.declarator.asVal().declarator.asVal() as IdentifierNode).name
-//    before.add("global $name")
-//    val instr = mutableListOf<String>()
-//    instr.add("push rbp")
-//    f.block.asVal().items.map { it.asVal() }.forEach {
-//      val item = when (it) {
-//        is Declaration -> TODO()
-//        is Statement -> genStatement(f, it)
-//        else -> TODO()
-//      }
-//      instr.add(item)
-//    }
-//    instr.add("pop rbp")
-//    text.add("$name:")
-//    text.add(instr.joinInstructions())
-//  }
+  private fun declRunInitializers(d: RealDeclaration): Instructions {
+    val instr = mutableListOf<String>()
+    if (d.declSpecs.typeSpec != TypeSpecifier.INT) TODO("only ints are implemented for now")
+    val inits = d.declaratorList.mapNotNull { it as? InitDeclarator }
+    if (inits.size > 6) TODO("all values are in registers, and there are not many of those")
+    inits.forEach {
+      val regIdx = registers.indexOfFirst { reg -> reg.itemName.isEmpty() }
+      if (regIdx == -1) TODO("out of registers?")
+      registers[regIdx].itemName = it.name
+      val resRegister = genExpr(it.initializer)
+      instr.add("mov ${registers[regIdx].name}, ${resRegister.name}")
+    }
+    return instr
+  }
+
+  private fun genBlock(block: CompoundStatement): Instructions {
+    val statements = block.items.mapNotNull { (it as? StatementItem)?.statement }
+    val directDecls = block.items.mapNotNull { (it as? DeclarationItem)?.it }
+    val nestedDecls = statements.mapNotNull {
+      when {
+        it is ForStatement && it.init is DeclarationInitializer -> it.init.value as RealDeclaration
+        // FIXME
+        else -> null
+      }
+    }
+    val decls = directDecls + nestedDecls
+    return decls.map { declRunInitializers(it) }.reduce { ac, it -> ac + it }
+    // FIXME: the rest of this function
+  }
+
+  private fun genFunction(f: FunctionDefinition) {
+    // FIXME: the declarator can also be a pointer+ident combo
+    prelude.add("global ${f.name}")
+    val instr = mutableListOf<String>()
+    instr.add("push rbp")
+    instr += genBlock(f.block)
+    instr.add("pop rbp")
+    text.add("${f.name}:")
+    text.add(instr.joinInstructions())
+  }
 
   fun getNasm(): String {
-    return before.joinToString("\n") +
+    return prelude.joinToString("\n") +
         "\nsection .data\n" +
         data.joinToString("\n") +
         "\nsection .text\n" +
--- src/test/kotlin/slak/test/CodegenTests.kt
+++ src/test/kotlin/slak/test/CodegenTests.kt
@@ -6,7 +6,12 @@ import slak.ckompiler.CodeGenerator
 class CodegenTests {
   @Test
   fun basic() {
-    val p = prepareCode("int main() {return 0;}", source)
+    val p = prepareCode("""
+      int main() {
+        int a = 1;
+        return 0;
+      }
+    """.trimIndent(), source)
     p.assertNoDiagnostics()
     val codegen = CodeGenerator(p.root)
     println(codegen.getNasm())
