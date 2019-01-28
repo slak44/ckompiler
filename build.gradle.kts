75fe32a964c429e3e73c4a0cac7c0868c86cdf66
Stefan Silviu <sylviu44@gmail.com>
2019-01-28 16:30:11 +0200

    Change gradle dependencies from old 'compile' to 'implementation'

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ repositories {
 }
 
 dependencies {
-  compile(kotlin("stdlib"))
-  compile(group = "com.github.Kotlin", name = "kotlinx.cli", version = "-SNAPSHOT")
-  compile(group = "org.slf4j", name = "slf4j-log4j12", version = "1.7.25")
-  compile(group = "io.github.microutils", name = "kotlin-logging", version = "1.6.10")
-  testCompile(kotlin("test-junit", "1.3.11"))
-  testCompile(group = "junit", name = "junit", version = "4.11")
+  implementation(kotlin("stdlib"))
+  implementation(group = "com.github.Kotlin", name = "kotlinx.cli", version = "-SNAPSHOT")
+  implementation(group = "org.slf4j", name = "slf4j-log4j12", version = "1.7.25")
+  implementation(group = "io.github.microutils", name = "kotlin-logging", version = "1.6.10")
+  testImplementation(kotlin("test-junit", "1.3.11"))
+  testImplementation(group = "junit", name = "junit", version = "4.11")
 }
