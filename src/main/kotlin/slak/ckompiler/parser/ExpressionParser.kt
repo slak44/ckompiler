088ef06fdf74f1c8242bb5bfbabf978c0dbc9020
Stefan Silviu <sylviu44@gmail.com>
2019-02-20 21:02:20 +0200

    More work on Expression types

--- src/main/kotlin/slak/ckompiler/parser/ExpressionParser.kt
+++ src/main/kotlin/slak/ckompiler/parser/ExpressionParser.kt
@@ -21,35 +21,7 @@ interface IExpressionParser {
 enum class UnaryOperators(val op: Punctuators) {
   REF(Punctuators.AMP), DEREF(Punctuators.STAR),
   PLUS(Punctuators.PLUS), MINUS(Punctuators.MINUS),
-  BIT_NOT(Punctuators.TILDE), NOT(Punctuators.NOT);
-
-  /**
-   * C standard: 6.5.3.3.1, 6.5.3.3.5
-   * @return the type of the expression after applying a unary operator ([ErrorType] if it can't be
-   * applied)
-   */
-  fun applyTo(target: TypeName): TypeName = when (this) {
-    UnaryOperators.PLUS, UnaryOperators.MINUS -> {
-      // FIXME: apply promotion rules
-      if (!target.isArithmetic()) ErrorType else SignedIntType
-    }
-    UnaryOperators.BIT_NOT -> {
-      // FIXME: apply promotion rules and do 6.5.3.3.4
-      if (target !is IntegralType) ErrorType else SignedIntType
-    }
-    UnaryOperators.NOT -> {
-      // The result of this operator is always `int`, as per 6.5.3.3.5
-      if (!target.isScalar()) ErrorType else SignedIntType
-    }
-    UnaryOperators.REF -> {
-      if (target is ErrorType) ErrorType
-      // FIXME: check ALL the constraints from 6.5.3.2.1
-      else PointerType(target, emptyList())
-    }
-    UnaryOperators.DEREF -> {
-      if (target !is PointerType) ErrorType else target.referencedType
-    }
-  }
+  BIT_NOT(Punctuators.TILDE), NOT(Punctuators.NOT)
 }
 
 /**
--- src/main/kotlin/slak/ckompiler/parser/TypeSystem.kt
+++ src/main/kotlin/slak/ckompiler/parser/TypeSystem.kt
@@ -1,5 +1,12 @@
 package slak.ckompiler.parser
 
+import mu.KotlinLogging
+import slak.ckompiler.throwICE
+import slak.ckompiler.parser.BinaryOperators.*
+import slak.ckompiler.parser.UnaryOperators.*
+
+private val logger = KotlinLogging.logger("TypeSystem")
+
 fun typeNameOfTag(tagSpecifier: TagSpecifier): TypeName {
   val tagName = if (tagSpecifier.isAnonymous) null else tagSpecifier.tagIdent.name
 //  val tagDef = if (tagSpecifier.isAnonymous) null else searchTag(tagSpecifier.tagIdent)
@@ -134,7 +141,29 @@ data class UnionType(val name: String?, val optionTypes: List<TypeName>?) : Type
 
 sealed class BasicType : TypeName()
 
-sealed class IntegralType : BasicType()
+sealed class IntegralType : BasicType(), Comparable<IntegralType> {
+  /** C standard: 6.3.1.1.0.1 */
+  abstract val conversionRank: Int
+  /**
+   * The same type with opposite signedness. Examples:
+   * [SignedLongLongType] -> [UnsignedLongLongType]
+   * [UnsignedCharType] -> [SignedCharType]
+   * [BooleanType] -> throws ICE
+   */
+  abstract val corespondingType: IntegralType
+  /**
+   * If integer promotions are applied to this type, [promotedType] is the resulting type.
+   *
+   * FIXME: bitfields are not handled
+   * FIXME: the results of these promotions can depend on the size of the types
+   *
+   * C standard: 6.3.1.1.0.2
+   */
+  abstract val promotedType: IntegralType
+
+  /** @see conversionRank */
+  override operator fun compareTo(other: IntegralType) = other.conversionRank - this.conversionRank
+}
 
 /**
  * We consider character types to be integral, and char to behave as signed char.
@@ -144,22 +173,37 @@ sealed class IntegralType : BasicType()
 sealed class SignedIntegralType : IntegralType()
 
 object SignedCharType : SignedIntegralType() {
+  override val conversionRank = 0x0000F
+  override val corespondingType = UnsignedCharType
+  override val promotedType = SignedIntType
   override fun toString() = "signed char"
 }
 
 object SignedShortType : SignedIntegralType() {
+  override val conversionRank = 0x000F0
+  override val corespondingType = UnsignedShortType
+  override val promotedType = SignedIntType
   override fun toString() = "signed short"
 }
 
 object SignedIntType : SignedIntegralType() {
+  override val conversionRank = 0x00F00
+  override val corespondingType = UnsignedIntType
+  override val promotedType = SignedIntType
   override fun toString() = "signed int"
 }
 
 object SignedLongType : SignedIntegralType() {
+  override val conversionRank = 0x0F000
+  override val corespondingType = UnsignedLongType
+  override val promotedType = SignedLongType
   override fun toString() = "signed long"
 }
 
 object SignedLongLongType : SignedIntegralType() {
+  override val conversionRank = 0xF0000
+  override val corespondingType = UnsignedLongLongType
+  override val promotedType = SignedLongLongType
   override fun toString() = "signed long long"
 }
 
@@ -167,26 +211,44 @@ object SignedLongLongType : SignedIntegralType() {
 sealed class UnsignedIntegralType : IntegralType()
 
 object BooleanType : UnsignedIntegralType() {
+  override val conversionRank = 0x00001
+  override val corespondingType = logger.throwICE("No signed corespondent for _Bool") {}
+  override val promotedType = SignedIntType
   override fun toString() = "_Bool"
 }
 
 object UnsignedCharType : UnsignedIntegralType() {
+  override val conversionRank = SignedCharType.conversionRank
+  override val corespondingType = SignedCharType
+  override val promotedType = SignedIntType
   override fun toString() = "unsigned char"
 }
 
 object UnsignedShortType : UnsignedIntegralType() {
+  override val conversionRank = SignedShortType.conversionRank
+  override val corespondingType = SignedShortType
+  override val promotedType = SignedIntType
   override fun toString() = "unsigned short"
 }
 
 object UnsignedIntType : UnsignedIntegralType() {
+  override val conversionRank = SignedIntType.conversionRank
+  override val corespondingType = SignedIntType
+  override val promotedType = UnsignedIntType
   override fun toString() = "unsigned int"
 }
 
 object UnsignedLongType : UnsignedIntegralType() {
+  override val conversionRank = SignedLongType.conversionRank
+  override val corespondingType = SignedLongType
+  override val promotedType = UnsignedLongType
   override fun toString() = "unsigned long"
 }
 
 object UnsignedLongLongType : UnsignedIntegralType() {
+  override val conversionRank = SignedLongLongType.conversionRank
+  override val corespondingType = SignedLongLongType
+  override val promotedType = UnsignedLongLongType
   override fun toString() = "unsigned long long"
 }
 
@@ -208,3 +270,108 @@ object LongDoubleType : FloatingType() {
 object VoidType : BasicType() {
   override fun toString() = "void"
 }
+
+/**
+ * Applies the usual arithmetic conversions from the standard. Propagates [ErrorType].
+ *
+ * Throws if either type is not arithmetic.
+ *
+ * C standard: 6.3.1.8
+ * @return the "common real type" everything gets converted to
+ */
+fun usualArithmeticConversions(lhs: TypeName, rhs: TypeName): TypeName {
+  if (lhs is ErrorType || rhs is ErrorType) return ErrorType
+  if (!lhs.isArithmetic() || !rhs.isArithmetic()) {
+    logger.throwICE("Applying arithmetic conversions on non-arithmetic operands") {
+      "lhs: $lhs, rhs: $rhs"
+    }
+  }
+  if (lhs is LongDoubleType || rhs is LongDoubleType) return LongDoubleType
+  if (lhs is DoubleType || rhs is DoubleType) return DoubleType
+  if (lhs is FloatType || rhs is FloatType) return FloatType
+  val lInt = (lhs as IntegralType).promotedType
+  val rInt = (rhs as IntegralType).promotedType
+  if (lInt.javaClass == rInt.javaClass) return lhs
+  val big = maxOf(lInt, rInt)
+  val small = minOf(lInt, rInt)
+  // Check for same signed-ness
+  if (lInt is UnsignedIntegralType == rInt is UnsignedIntegralType) return big
+  if (big is UnsignedIntegralType) return big
+  big as SignedIntegralType
+  // Check if big is *strictly* bigger
+  if (big > small) return big
+  return big.corespondingType
+}
+
+/**
+ * C standard: 6.5.3.3.1, 6.5.3.3.5
+ * @return the type of the expression after applying a unary operator ([ErrorType] if it can't be
+ * applied)
+ */
+fun UnaryOperators.applyTo(target: TypeName): TypeName = when (this) {
+  PLUS, MINUS -> {
+    // FIXME: apply promotion rules
+    if (!target.isArithmetic()) ErrorType else SignedIntType
+  }
+  BIT_NOT -> {
+    // FIXME: apply promotion rules and do 6.5.3.3.4
+    if (target !is IntegralType) ErrorType else SignedIntType
+  }
+  NOT -> {
+    // The result of this operator is always `int`, as per 6.5.3.3.5
+    if (!target.isScalar()) ErrorType else SignedIntType
+  }
+  REF -> {
+    if (target is ErrorType) ErrorType
+    // FIXME: check ALL the constraints from 6.5.3.2.1
+    else PointerType(target, emptyList())
+  }
+  DEREF -> {
+    if (target !is PointerType) ErrorType else target.referencedType
+  }
+}
+
+/**
+ * C standard: 6.5.5.0.2
+ * @return the type of the expression after applying the binary operator on the operands
+ * ([ErrorType] if it can't be applied)
+ * FIXME: promotions and conversions
+ */
+fun BinaryOperators.applyTo(lhs: TypeName, rhs: TypeName): TypeName = when (this) {
+  MUL, DIV -> {
+    if (!lhs.isArithmetic() || !rhs.isArithmetic()) ErrorType
+    else usualArithmeticConversions(lhs, rhs)
+  }
+  MOD -> {
+    if (lhs !is IntegralType || rhs !is IntegralType) ErrorType
+    else usualArithmeticConversions(lhs, rhs)
+  }
+  ADD -> TODO()
+  SUB -> TODO()
+  LSH -> TODO()
+  RSH -> TODO()
+  LT -> TODO()
+  GT -> TODO()
+  LEQ -> TODO()
+  GEQ -> TODO()
+  EQ -> TODO()
+  NEQ -> TODO()
+  BIT_AND -> TODO()
+  BIT_XOR -> TODO()
+  BIT_OR -> TODO()
+  AND -> TODO()
+  OR -> TODO()
+  ASSIGN -> TODO()
+  MUL_ASSIGN -> TODO()
+  DIV_ASSIGN -> TODO()
+  MOD_ASSIGN -> TODO()
+  PLUS_ASSIGN -> TODO()
+  SUB_ASSIGN -> TODO()
+  LSH_ASSIGN -> TODO()
+  RSH_ASSIGN -> TODO()
+  AND_ASSIGN -> TODO()
+  XOR_ASSIGN -> TODO()
+  OR_ASSIGN -> TODO()
+  COMMA -> TODO()
+}
+
