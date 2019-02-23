6a2df76a356e10b7b215873f461451104ee1f145
Stefan Silviu <sylviu44@gmail.com>
2019-02-23 12:11:58 +0200

    Improve CFG tests

--- src/test/kotlin/slak/test/analysis/CFGTests.kt
+++ src/test/kotlin/slak/test/analysis/CFGTests.kt
@@ -1,9 +1,7 @@
 package slak.test.analysis
 
 import org.junit.Test
-import slak.ckompiler.analysis.Return
-import slak.ckompiler.analysis.createGraphFor
-import slak.ckompiler.analysis.createGraphviz
+import slak.ckompiler.analysis.*
 import slak.ckompiler.parser.ExternalDeclaration
 import slak.ckompiler.parser.FunctionDefinition
 import slak.test.assertNoDiagnostics
@@ -13,6 +11,16 @@ import slak.test.source
 
 private fun List<ExternalDeclaration>.firstFun(): FunctionDefinition =
     first { it is FunctionDefinition } as FunctionDefinition
+
+private val BasicBlock.ret: Return get() {
+  assert(terminator is Return)
+  return terminator as Return
+}
+
+private fun Return.assertHasDeadCode() {
+  assert(deadCode != null)
+  assert(deadCode!!.data.isNotEmpty() || deadCode!!.isTerminated())
+}
 
 class CFGTests {
   @Test
@@ -47,9 +55,25 @@ class CFGTests {
     p.assertNoDiagnostics()
     val startBlock = createGraphFor(p.root.decls.firstFun())
     assert(startBlock.data.isNotEmpty())
-    assert(startBlock.terminator is Return)
-    val ret = startBlock.terminator as Return
-    assert(ret.deadCode != null)
-    assert(ret.deadCode!!.data.isNotEmpty())
+    startBlock.ret.assertHasDeadCode()
+  }
+
+  @Test
+  fun `Dead Code After Terminal If`() {
+    val p = prepareCode("""
+      int main() {
+        int x = 1;
+        if (x < 1) return 7;
+        else return 8;
+        int dead = 265;
+      }
+    """.trimIndent(), source)
+    p.assertNoDiagnostics()
+    val startBlock = createGraphFor(p.root.decls.firstFun())
+    assert(startBlock.data.isNotEmpty())
+    assert(startBlock.terminator is CondJump)
+    val ifJmp = startBlock.terminator as CondJump
+    ifJmp.target.ret.assertHasDeadCode()
+    ifJmp.other.ret.assertHasDeadCode()
   }
 }
