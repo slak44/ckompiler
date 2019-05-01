5257c1404202a838afd0c8580c8f5134dfc80ee3
Stefan Silviu <sylviu44@gmail.com>
2019-05-01 19:22:30 +0300

    Fix whitespace

--- src/main/kotlin/slak/ckompiler/parser/SyntaxTreeModel.kt
+++ src/main/kotlin/slak/ckompiler/parser/SyntaxTreeModel.kt
@@ -180,7 +180,7 @@ class ErrorExpression : Expression(), ErrorNode by ErrorNodeImpl {
  * the rest of the AST.
  */
 class TypedIdentifier(override val name: String,
-                           override val type: TypeName) : Expression(), OrdinaryIdentifier {
+                      override val type: TypeName) : Expression(), OrdinaryIdentifier {
   constructor(ds: DeclarationSpecifier,
               decl: NamedDeclarator) : this(decl.name.name, typeNameOf(ds, decl)) {
     withRange(decl.name.tokenRange)
