c672a757571f3d1cb69a5b0d26faa7250cd85805
Stefan Silviu <sylviu44@gmail.com>
2019-07-31 19:37:48 +0300

    Add FIXME

--- src/main/kotlin/slak/ckompiler/backend/nasm_x86_64/NasmGenerator.kt
+++ src/main/kotlin/slak/ckompiler/backend/nasm_x86_64/NasmGenerator.kt
@@ -177,6 +177,7 @@ class NasmGenerator(externals: List<String>, functions: List<CFG>, mainCfg: CFG?
     is ImpossibleJump -> genReturn(jmp.returned)
     is ConstantJump -> genUncondJump(jmp.target)
     // FIXME: handle the case where the function is main, and the final block is allowed to be this
+    //   alternatively, fix this in the parser by handling main separately
     MissingJump -> logger.throwICE("Incomplete BasicBlock")
   }
 
