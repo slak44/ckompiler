f4c11e31b20a4448838125cc9a5d2b0d72d14a0b
Stefan Silviu <sylviu44@gmail.com>
2019-08-18 08:38:33 +0300

    Rewrite test DSL for E2E tests

--- src/test/kotlin/slak/test/backend/NasmTests.kt
+++ src/test/kotlin/slak/test/backend/NasmTests.kt
@@ -25,20 +25,54 @@ class NasmTests {
     return asm
   }
 
-  /**
-   * Compiles and executes code. Returns exit code and stdout of executed code.
-   */
-  private fun compileAndRun(resource: File): Pair<Int, String> {
-    val (_, compilerExitCode) =
-        cli(resource.absolutePath, "-isystem", resource("include").absolutePath)
+  private class CompileAndRunBuilder {
+    var text: String? = null
+    var file: File? = null
+    var programArgList = listOf<String>()
+    var stdin: String? = null
+  }
+
+  private data class RunResult(val exitCode: Int, val stdout: String, val stderr: String)
+
+  private fun RunResult.justExitCode(expected: Int) {
+    assertEquals(expected, expected)
+    assertEquals("", stdout)
+    assertEquals("", stderr)
+  }
+
+  private fun compileAndRun(block: CompileAndRunBuilder.() -> Unit): RunResult {
+    val builder = CompileAndRunBuilder()
+    builder.block()
+    if (builder.text != null) System.setIn(builder.text!!.byteInputStream())
+    val target = if (builder.text != null) "-" else builder.file!!.absolutePath
+    val (_, compilerExitCode) = cli(target, "-isystem", resource("include").absolutePath)
     assertEquals(ExitCodes.NORMAL, compilerExitCode)
     assertTrue(File("a.out").exists())
-    val process = ProcessBuilder("./a.out")
-        .inheritIO()
+    val inputRedirect =
+        if (builder.stdin != null) ProcessBuilder.Redirect.PIPE else ProcessBuilder.Redirect.INHERIT
+    val process = ProcessBuilder("./a.out", *builder.programArgList.toTypedArray())
+        .redirectInput(inputRedirect)
         .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
         .start()
+    if (builder.stdin != null) {
+      process.outputStream.bufferedWriter().use {
+        it.write(builder.stdin!!)
+      }
+    }
     val stdout = process.inputStream.bufferedReader().readText()
-    return process.waitFor() to stdout
+    val stderr = process.errorStream.bufferedReader().readText()
+    val exitCode = process.waitFor()
+    return RunResult(exitCode = exitCode, stdout = stdout, stderr = stderr)
+  }
+
+  private fun compileAndRun(resource: File): RunResult = compileAndRun { file = resource }
+
+  private fun compileAndRun(code: String, programArgs: List<String> = emptyList()): RunResult {
+    return compileAndRun {
+      text = code
+      programArgList = programArgs
+    }
   }
 
   private val stdin = System.`in`
@@ -46,22 +80,6 @@ class NasmTests {
   @AfterEach
   fun restoreStdin() {
     System.setIn(stdin)
-  }
-
-  /**
-   * Compiles and executes code. Returns exit code and stdout of executed code.
-   */
-  private fun compileAndRun(code: String, args: List<String> = emptyList()): Pair<Int, String> {
-    System.setIn(code.byteInputStream())
-    val (_, compilerExitCode) = cli("-", "-isystem", resource("include").absolutePath)
-    assertEquals(ExitCodes.NORMAL, compilerExitCode)
-    assertTrue(File("a.out").exists())
-    val process = ProcessBuilder("./a.out", *args.toTypedArray())
-        .inheritIO()
-        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .start()
-    val stdout = process.inputStream.bufferedReader().readText()
-    return process.waitFor() to stdout
   }
 
   @AfterEach
@@ -135,95 +153,107 @@ class NasmTests {
   @ValueSource(strings = ["1", "1 2", "1 2 3", "1 2 3 4"])
   fun `Returns Argc`(cmdLine: String) {
     val args = cmdLine.split(" ")
-    assertEquals(args.size + 1 to "", compileAndRun("int main(int argc) { return argc; }", args))
+    compileAndRun("int main(int argc) { return argc; }", args).run {
+      assertEquals(args.size + 1, exitCode)
+      assertEquals("", stdout)
+      assertEquals("", stderr)
+    }
   }
 
   @Test
   fun `Exit Code 10`() {
-    assertEquals(10 to "", compileAndRun(resource("e2e/returns10.c")))
+    compileAndRun(resource("e2e/returns10.c")).justExitCode(10)
   }
 
   @Test
   fun `Exit Code Sum`() {
-    assertEquals(2 to "", compileAndRun(resource("e2e/returns1+1.c")))
+    compileAndRun(resource("e2e/returns1+1.c")).justExitCode(2)
   }
 
   @Test
   fun `Simple If With False Condition`() {
-    assertEquals(0 to "", compileAndRun(resource("e2e/simpleIf.c")))
+    compileAndRun(resource("e2e/simpleIf.c")).justExitCode(0)
   }
 
   @Test
   fun `If With Variable As Condition`() {
-    assertEquals(1 to "", compileAndRun(resource("e2e/cmpVariable.c")))
+    compileAndRun(resource("e2e/cmpVariable.c")).justExitCode(1)
   }
 
   @Test
   fun `Hello World!`() {
-    assertEquals(0 to "Hello World!\n", compileAndRun(resource("e2e/helloWorld.c")))
+    compileAndRun(resource("e2e/helloWorld.c")).run {
+      assertEquals(0, exitCode)
+      assertEquals("Hello World!\n", stdout)
+      assertEquals("", stderr)
+    }
   }
 
   @Test
   fun `Float Ops Test`() {
-    assertEquals(0 to "", compileAndRun(resource("e2e/floatOps.c")))
+    compileAndRun(resource("e2e/floatOps.c")).justExitCode(0)
   }
 
   @ParameterizedTest
   @ValueSource(strings = ["!0", "!(1-1)"])
   fun `Unary Not`(code: String) {
-    assertEquals(1 to "", compileAndRun("int main() { return $code; }"))
+    compileAndRun("int main() { return $code; }").justExitCode(1)
   }
 
   @ParameterizedTest
   @ValueSource(strings = ["(int) 1.1F", "(int) 1.0F", "(int) 1.99F"])
   fun `Float Cast To Int`(code: String) {
-    assertEquals(1 to "", compileAndRun("int main() { return $code; }"))
+    compileAndRun("int main() { return $code; }").justExitCode(1)
   }
 
   @ParameterizedTest
   @ValueSource(strings = ["1", "123", "-1"])
   fun `Int Cast To Float`(int: String) {
-    assertEquals(0 to "$int.00", compileAndRun("""
+    compileAndRun("""
       extern int printf(const char*, ...);
       int main() {
         printf("%.2f", (float) $int);
         return 0;
       }
-    """.trimIndent()))
+    """.trimIndent()).run {
+      assertEquals(0, exitCode)
+      assertEquals("$int.00", stdout)
+      assertEquals("", stderr)
+    }
   }
 
   @Test
   fun `Int Pointers Referencing And Dereferencing`() {
-    assertEquals(12 to "", compileAndRun("""
+    compileAndRun("""
       int main() {
         int a = 12;
         int* b = &a;
         int c = *b;
         return c;
       }
-    """.trimIndent()))
+    """.trimIndent()).justExitCode(12)
   }
 
   @Disabled("fix dereferencing and synthetic store targets first")
   @Test
   fun `Simple Array Usage`() {
-    assertEquals(12 to "", compileAndRun("""
+    compileAndRun("""
       int main() {
         int a[2];
         a[0] = 12;
         a[1] = 13;
         return a[0];
       }
-    """.trimIndent()))
+    """.trimIndent()).justExitCode(12)
   }
 
   @Test
   fun `Ternary Test`() {
-    assertEquals(13 to "", compileAndRun(resource("e2e/ternaryOps.c")))
+    compileAndRun(resource("e2e/ternaryOps.c")).justExitCode(13)
   }
 
   @Test
   fun `Early Return In Void Function Works`() {
-    assertEquals(0 to "", compileAndRun(resource("e2e/earlyReturn.c")))
+    compileAndRun(resource("e2e/earlyReturn.c")).justExitCode(0)
   }
 }
