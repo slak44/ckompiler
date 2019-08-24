daefecf3b532d4bb4d4aa3f7d4915ae369195771
Stefan Silviu <sylviu44@gmail.com>
2019-08-24 19:06:14 +0300

    Add more math.h prototypes

--- stdlib/include/math.h
+++ stdlib/include/math.h
@@ -1,5 +1,13 @@
 // C standard: 7.12.0.4
 #define INFINITY (1.0 / 0.0)
+// C standard: 7.12.7.4
+double pow(double x, double y);
+float powf(float x, float y);
+long double powl(long double x, long double y);
+// C standard: 7.12.7.5
+double sqrt(double x);
+float sqrtf(float x);
+long double sqrtl(long double x);
 // C standard: 7.12.9.6
 extern double round(double x);
 extern float roundf(float x);
