31744b2eead241e6098fe27212c491a1c34c5d83
Stefan Silviu <sylviu44@gmail.com>
2019-09-01 13:16:02 +0300

    Extract constant evaluation to separate file

---
+++ src/main/kotlin/slak/ckompiler/analysis/ExpressionEvaluator.kt
@@ -0,0 +1,182 @@
+package slak.ckompiler.analysis
+
+import org.apache.logging.log4j.LogManager
+import slak.ckompiler.lexer.FloatingSuffix
+import slak.ckompiler.lexer.IntegralSuffix
+import slak.ckompiler.parser.*
+import slak.ckompiler.throwICE
+
+private val logger = LogManager.getLogger("ExpressionEvaluator")
+
+fun convertToFloat(e: ExprConstantNode): Double = when (e) {
+  is IntegerConstantNode -> e.value.toDouble()
+  is FloatingConstantNode -> e.value
+  is CharacterConstantNode -> e.char.toDouble()
+  is ErrorExpression, is VoidExpression,
+  is StringLiteralNode -> logger.throwICE("This is not allowed here")
+}
+
+fun convertToInt(e: ExprConstantNode): Long = when (e) {
+  is IntegerConstantNode -> e.value
+  is FloatingConstantNode -> e.value.toLong()
+  is CharacterConstantNode -> e.char.toLong()
+  is ErrorExpression, is VoidExpression,
+  is StringLiteralNode -> logger.throwICE("This is not allowed here")
+}
+
+fun evalBasicArithmetic(
+    lhs: ExprConstantNode,
+    rhs: ExprConstantNode,
+    op: BinaryOperators,
+    resType: TypeName
+) = when (resType) {
+  is FloatingType -> {
+    val lhsConst = convertToFloat(lhs)
+    val rhsConst = convertToFloat(rhs)
+    val result = when (op) {
+      BinaryOperators.MUL -> lhsConst * rhsConst
+      BinaryOperators.DIV -> lhsConst / rhsConst
+      BinaryOperators.ADD -> lhsConst + rhsConst
+      BinaryOperators.SUB -> lhsConst - rhsConst
+      else -> logger.throwICE("Logically impossible condition")
+    }
+    FloatingConstantNode(result, FloatingSuffix.NONE)
+  }
+  is IntegralType -> {
+    val lhsConst = convertToInt(lhs)
+    val rhsConst = convertToInt(rhs)
+    val result = when (op) {
+      BinaryOperators.MUL -> lhsConst * rhsConst
+      BinaryOperators.DIV -> lhsConst / rhsConst
+      BinaryOperators.ADD -> lhsConst + rhsConst
+      BinaryOperators.SUB -> lhsConst - rhsConst
+      else -> logger.throwICE("Logically impossible condition")
+    }
+    IntegerConstantNode(result, IntegralSuffix.NONE)
+  }
+  else -> TODO("unknown result type")
+}
+
+// Just because it has the same syntax, it doesn't mean it does the same thing
+@Suppress("DuplicatedCode")
+fun evalBasicComparable(
+    lhs: ExprConstantNode,
+    rhs: ExprConstantNode,
+    op: BinaryOperators,
+    resType: TypeName
+) = when (resType) {
+  is FloatingType -> {
+    val lhsConst = convertToFloat(lhs)
+    val rhsConst = convertToFloat(rhs)
+    val result = when (op) {
+      BinaryOperators.LT -> lhsConst < rhsConst
+      BinaryOperators.GT -> lhsConst > rhsConst
+      BinaryOperators.LEQ -> lhsConst <= rhsConst
+      BinaryOperators.GEQ -> lhsConst >= rhsConst
+      BinaryOperators.EQ -> lhsConst == rhsConst
+      BinaryOperators.NEQ -> lhsConst != rhsConst
+      else -> logger.throwICE("Logically impossible condition")
+    }
+    IntegerConstantNode(if (result) 1L else 0L)
+  }
+  is IntegralType -> {
+    val lhsConst = convertToInt(lhs)
+    val rhsConst = convertToInt(rhs)
+    val result = when (op) {
+      BinaryOperators.LT -> lhsConst < rhsConst
+      BinaryOperators.GT -> lhsConst > rhsConst
+      BinaryOperators.LEQ -> lhsConst <= rhsConst
+      BinaryOperators.GEQ -> lhsConst >= rhsConst
+      BinaryOperators.EQ -> lhsConst == rhsConst
+      BinaryOperators.NEQ -> lhsConst != rhsConst
+      else -> logger.throwICE("Logically impossible condition")
+    }
+    IntegerConstantNode(if (result) 1L else 0L)
+  }
+  else -> TODO("unknown result type")
+}
+
+fun evalIntOnlyOps(
+    lhs: ExprConstantNode,
+    rhs: ExprConstantNode,
+    op: BinaryOperators
+): IntegerConstantNode {
+  val lhsConst = convertToInt(lhs)
+  val rhsConst = convertToInt(rhs)
+  val result = when (op) {
+    BinaryOperators.MOD -> lhsConst % rhsConst
+    BinaryOperators.LSH -> lhsConst shl rhsConst.toInt()
+    BinaryOperators.RSH -> lhsConst shr rhsConst.toInt()
+    BinaryOperators.BIT_AND -> lhsConst and rhsConst
+    BinaryOperators.BIT_XOR -> lhsConst xor rhsConst
+    BinaryOperators.BIT_OR -> lhsConst or rhsConst
+    BinaryOperators.AND -> (lhsConst != 0L && rhsConst != 0L).let { if (it) 1L else 0L }
+    BinaryOperators.OR -> (lhsConst != 0L || rhsConst != 0L).let { if (it) 1L else 0L }
+    else -> logger.throwICE("Logically impossible condition")
+  }
+  return IntegerConstantNode(result)
+}
+
+fun evalBinary(
+    lhs: ExprConstantNode,
+    rhs: ExprConstantNode,
+    op: BinaryOperators,
+    resType: TypeName
+): ExprConstantNode = when (op) {
+  BinaryOperators.LT, BinaryOperators.GT, BinaryOperators.LEQ, BinaryOperators.GEQ,
+  BinaryOperators.EQ, BinaryOperators.NEQ -> {
+    evalBasicComparable(lhs, rhs, op, resType)
+  }
+  BinaryOperators.MUL, BinaryOperators.DIV, BinaryOperators.ADD, BinaryOperators.SUB -> {
+    evalBasicArithmetic(lhs, rhs, op, resType)
+  }
+  BinaryOperators.MOD, BinaryOperators.LSH, BinaryOperators.RSH, BinaryOperators.BIT_AND,
+  BinaryOperators.BIT_XOR, BinaryOperators.BIT_OR, BinaryOperators.AND, BinaryOperators.OR -> {
+    evalIntOnlyOps(lhs, rhs, op)
+  }
+  else -> logger.throwICE("Illegal constant binary operator") { op }
+}
+
+fun evalUnary(
+    operand: ExprConstantNode,
+    op: UnaryOperators
+): ExprConstantNode = when (operand.type) {
+  is FloatingType -> {
+    val toApply = convertToFloat(operand)
+    when (op) {
+      UnaryOperators.REF, UnaryOperators.DEREF, UnaryOperators.BIT_NOT -> {
+        logger.throwICE("Illegal constant unary operator") { op }
+      }
+      UnaryOperators.PLUS -> FloatingConstantNode(+toApply, FloatingSuffix.NONE)
+      UnaryOperators.MINUS -> FloatingConstantNode(-toApply, FloatingSuffix.NONE)
+      UnaryOperators.NOT -> IntegerConstantNode(if (toApply != 0.0) 1L else 0L)
+    }
+  }
+  is IntegralType -> {
+    val toApply = convertToInt(operand)
+    val result = when (op) {
+      UnaryOperators.REF, UnaryOperators.DEREF -> {
+        logger.throwICE("Illegal constant unary operator") { op }
+      }
+      UnaryOperators.PLUS -> +toApply
+      UnaryOperators.MINUS -> -toApply
+      UnaryOperators.BIT_NOT -> toApply.inv()
+      UnaryOperators.NOT -> if (toApply != 0L) 0L else 1L
+    }
+    IntegerConstantNode(result)
+  }
+  else -> TODO("unknown result type")
+}
+
+fun evalCast(type: TypeName, target: ExprConstantNode): ExprConstantNode = when (type) {
+  ErrorType -> ErrorExpression().withRange(target)
+  VoidType -> VoidExpression().withRange(target)
+  is IntegralType -> IntegerConstantNode(convertToInt(target))
+  is FloatingType -> FloatingConstantNode(convertToFloat(target), FloatingSuffix.NONE)
+  is PointerType -> TODO()
+  is FunctionType -> TODO()
+  is ArrayType -> TODO()
+  is BitfieldType -> TODO()
+  is StructureType -> TODO()
+  is UnionType -> TODO()
+}
--- src/main/kotlin/slak/ckompiler/parser/ConstantExprParser.kt
+++ src/main/kotlin/slak/ckompiler/parser/ConstantExprParser.kt
@@ -1,8 +1,9 @@
 package slak.ckompiler.parser
 
 import slak.ckompiler.*
-import slak.ckompiler.lexer.FloatingSuffix
-import slak.ckompiler.lexer.IntegralSuffix
+import slak.ckompiler.analysis.evalBinary
+import slak.ckompiler.analysis.evalCast
+import slak.ckompiler.analysis.evalUnary
 
 object ConstExprIdents : IdentSearchable, TypeNameParser {
   override fun parseTypeName(endIdx: Int): TypeName? {
@@ -33,11 +34,7 @@ interface IConstantExprParser : IDebugHandler {
   fun evaluateExpr(expr: Expression): Pair<ExprConstantNode, List<Diagnostic>>
 }
 
-/**
- * FIXME: the evaluation of constant expressions cuts A LOT of corners
- *
- * C standard: 6.6
- */
+/** C standard: 6.6 */
 class ConstantExprParser(val type: ConstantExprType, exprParser: ExpressionParser) :
     IConstantExprParser,
     IDebugHandler by exprParser,
@@ -104,7 +101,7 @@ class ConstantExprParser(val type: ConstantExprType, exprParser: ExpressionParse
             " recognized in preprocessor, so casts can't either")
       }
       ConstantExprType.DECLARATOR_ARRAY_SIZE -> {
-        doCast(expr.type, evaluateExprImpl(diags, expr.target))
+        evalCast(expr.type, evaluateExprImpl(diags, expr.target))
       }
     }
     is UnaryExpression -> {
@@ -118,7 +115,7 @@ class ConstantExprParser(val type: ConstantExprType, exprParser: ExpressionParse
           ErrorExpression().withRange(expr)
         }
         operand.type is ErrorType -> ErrorExpression().withRange(expr)
-        else -> doUnary(operand, expr.op)
+        else -> evalUnary(operand, expr.op)
       }
     }
     is BinaryExpression -> {
@@ -135,172 +132,9 @@ class ConstantExprParser(val type: ConstantExprType, exprParser: ExpressionParse
         expr.type is ErrorType || lhs.type is ErrorType || rhs.type is ErrorType -> {
           ErrorExpression().withRange(expr)
         }
-        else -> doBinary(lhs, rhs, expr.op, expr.type)
+        else -> evalBinary(lhs, rhs, expr.op, expr.type)
       }
     }
     is SizeofTypeName -> TODO("deal with this after we implement sizeof")
-  }
-
-  private fun convertToFloat(e: ExprConstantNode): Double = when (e) {
-    is IntegerConstantNode -> e.value.toDouble()
-    is FloatingConstantNode -> e.value
-    is CharacterConstantNode -> e.char.toDouble()
-    is ErrorExpression, is VoidExpression,
-    is StringLiteralNode -> logger.throwICE("This is not allowed here")
-  }
-
-  private fun convertToInt(e: ExprConstantNode): Long = when (e) {
-    is IntegerConstantNode -> e.value
-    is FloatingConstantNode -> e.value.toLong()
-    is CharacterConstantNode -> e.char.toLong()
-    is ErrorExpression, is VoidExpression,
-    is StringLiteralNode -> logger.throwICE("This is not allowed here")
-  }
-
-  private fun basicArithm(lhs: ExprConstantNode,
-                          rhs: ExprConstantNode,
-                          op: BinaryOperators,
-                          resType: TypeName) = when (resType) {
-    is FloatingType -> {
-      val lhsConst = convertToFloat(lhs)
-      val rhsConst = convertToFloat(rhs)
-      val result = when (op) {
-        BinaryOperators.MUL -> lhsConst * rhsConst
-        BinaryOperators.DIV -> lhsConst / rhsConst
-        BinaryOperators.ADD -> lhsConst + rhsConst
-        BinaryOperators.SUB -> lhsConst - rhsConst
-        else -> logger.throwICE("Logically impossible condition")
-      }
-      FloatingConstantNode(result, FloatingSuffix.NONE)
-    }
-    is IntegralType -> {
-      val lhsConst = convertToInt(lhs)
-      val rhsConst = convertToInt(rhs)
-      val result = when (op) {
-        BinaryOperators.MUL -> lhsConst * rhsConst
-        BinaryOperators.DIV -> lhsConst / rhsConst
-        BinaryOperators.ADD -> lhsConst + rhsConst
-        BinaryOperators.SUB -> lhsConst - rhsConst
-        else -> logger.throwICE("Logically impossible condition")
-      }
-      IntegerConstantNode(result, IntegralSuffix.NONE)
-    }
-    else -> TODO("unknown result type")
-  }
-
-  // Just because it has the same syntax, it doesn't mean it does the same thing
-  @Suppress("DuplicatedCode")
-  private fun basicComparable(lhs: ExprConstantNode,
-                              rhs: ExprConstantNode,
-                              op: BinaryOperators,
-                              resType: TypeName) = when (resType) {
-    is FloatingType -> {
-      val lhsConst = convertToFloat(lhs)
-      val rhsConst = convertToFloat(rhs)
-      val result = when (op) {
-        BinaryOperators.LT -> lhsConst < rhsConst
-        BinaryOperators.GT -> lhsConst > rhsConst
-        BinaryOperators.LEQ -> lhsConst <= rhsConst
-        BinaryOperators.GEQ -> lhsConst >= rhsConst
-        BinaryOperators.EQ -> lhsConst == rhsConst
-        BinaryOperators.NEQ -> lhsConst != rhsConst
-        else -> logger.throwICE("Logically impossible condition")
-      }
-      IntegerConstantNode(if (result) 1L else 0L)
-    }
-    is IntegralType -> {
-      val lhsConst = convertToInt(lhs)
-      val rhsConst = convertToInt(rhs)
-      val result = when (op) {
-        BinaryOperators.LT -> lhsConst < rhsConst
-        BinaryOperators.GT -> lhsConst > rhsConst
-        BinaryOperators.LEQ -> lhsConst <= rhsConst
-        BinaryOperators.GEQ -> lhsConst >= rhsConst
-        BinaryOperators.EQ -> lhsConst == rhsConst
-        BinaryOperators.NEQ -> lhsConst != rhsConst
-        else -> logger.throwICE("Logically impossible condition")
-      }
-      IntegerConstantNode(if (result) 1L else 0L)
-    }
-    else -> TODO("unknown result type")
-  }
-
-  private fun intOnlyOps(lhs: ExprConstantNode,
-                         rhs: ExprConstantNode,
-                         op: BinaryOperators): IntegerConstantNode {
-    val lhsConst = convertToInt(lhs)
-    val rhsConst = convertToInt(rhs)
-    val result = when (op) {
-      BinaryOperators.MOD -> lhsConst % rhsConst
-      BinaryOperators.LSH -> lhsConst shl rhsConst.toInt()
-      BinaryOperators.RSH -> lhsConst shr rhsConst.toInt()
-      BinaryOperators.BIT_AND -> lhsConst and rhsConst
-      BinaryOperators.BIT_XOR -> lhsConst xor rhsConst
-      BinaryOperators.BIT_OR -> lhsConst or rhsConst
-      BinaryOperators.AND -> (lhsConst != 0L && rhsConst != 0L).let { if (it) 1L else 0L }
-      BinaryOperators.OR -> (lhsConst != 0L || rhsConst != 0L).let { if (it) 1L else 0L }
-      else -> logger.throwICE("Logically impossible condition")
-    }
-    return IntegerConstantNode(result)
-  }
-
-  private fun doBinary(lhs: ExprConstantNode,
-                       rhs: ExprConstantNode,
-                       op: BinaryOperators,
-                       resType: TypeName): ExprConstantNode = when (op) {
-    BinaryOperators.LT, BinaryOperators.GT, BinaryOperators.LEQ, BinaryOperators.GEQ,
-    BinaryOperators.EQ, BinaryOperators.NEQ -> {
-      basicComparable(lhs, rhs, op, resType)
-    }
-    BinaryOperators.MUL, BinaryOperators.DIV, BinaryOperators.ADD, BinaryOperators.SUB -> {
-      basicArithm(lhs, rhs, op, resType)
-    }
-    BinaryOperators.MOD, BinaryOperators.LSH, BinaryOperators.RSH, BinaryOperators.BIT_AND,
-    BinaryOperators.BIT_XOR, BinaryOperators.BIT_OR, BinaryOperators.AND, BinaryOperators.OR -> {
-      intOnlyOps(lhs, rhs, op)
-    }
-    else -> logger.throwICE("Illegal constant binary operator") { op }
-  }
-
-  private fun doUnary(operand: ExprConstantNode,
-                      op: UnaryOperators): ExprConstantNode = when (operand.type) {
-    is FloatingType -> {
-      val toApply = convertToFloat(operand)
-      when (op) {
-        UnaryOperators.REF, UnaryOperators.DEREF, UnaryOperators.BIT_NOT -> {
-          logger.throwICE("Illegal constant unary operator") { op }
-        }
-        UnaryOperators.PLUS -> FloatingConstantNode(+toApply, FloatingSuffix.NONE)
-        UnaryOperators.MINUS -> FloatingConstantNode(-toApply, FloatingSuffix.NONE)
-        UnaryOperators.NOT -> IntegerConstantNode(if (toApply != 0.0) 1L else 0L)
-      }
-    }
-    is IntegralType -> {
-      val toApply = convertToInt(operand)
-      val result = when (op) {
-        UnaryOperators.REF, UnaryOperators.DEREF -> {
-          logger.throwICE("Illegal constant unary operator") { op }
-        }
-        UnaryOperators.PLUS -> +toApply
-        UnaryOperators.MINUS -> -toApply
-        UnaryOperators.BIT_NOT -> toApply.inv()
-        UnaryOperators.NOT -> if (toApply != 0L) 0L else 1L
-      }
-      IntegerConstantNode(result)
-    }
-    else -> TODO("unknown result type")
-  }
-
-  private fun doCast(type: TypeName, target: ExprConstantNode): ExprConstantNode = when (type) {
-    ErrorType -> ErrorExpression().withRange(target)
-    VoidType -> VoidExpression().withRange(target)
-    is IntegralType -> IntegerConstantNode(convertToInt(target))
-    is FloatingType -> FloatingConstantNode(convertToFloat(target), FloatingSuffix.NONE)
-    is PointerType -> TODO()
-    is FunctionType -> TODO()
-    is ArrayType -> TODO()
-    is BitfieldType -> TODO()
-    is StructureType -> TODO()
-    is UnionType -> TODO()
   }
 }
