0df425bd75c9eda4ccddcd4d2a1bdf2a676c68be
Stefan Silviu <sylviu44@gmail.com>
2020-01-23 18:24:05 +0200

    Abstract reg alloc test code

--- src/test/kotlin/slak/test/backend/X64Tests.kt
+++ src/test/kotlin/slak/test/backend/X64Tests.kt
@@ -5,10 +5,7 @@ import slak.ckompiler.analysis.IntBinary
 import slak.ckompiler.analysis.IntConstant
 import slak.ckompiler.analysis.IntegralBinaryOps
 import slak.ckompiler.analysis.VirtualRegister
-import slak.ckompiler.backend.Memory
-import slak.ckompiler.backend.instructionSelection
-import slak.ckompiler.backend.regAlloc
-import slak.ckompiler.backend.stringify
+import slak.ckompiler.backend.*
 import slak.ckompiler.backend.x64.Imm
 import slak.ckompiler.backend.x64.ModRM
 import slak.ckompiler.backend.x64.X64InstrTemplate
@@ -21,6 +18,20 @@ import slak.test.source
 import kotlin.test.assertEquals
 
 class X64Tests {
+  private fun regAlloc(resourceName: String): Pair<InstructionMap, AllocationMap> {
+    val cfg = prepareCFG(resource(resourceName), source)
+    val (newLists, allocs) = X64Target.regAlloc(cfg, X64Target.instructionSelection(cfg))
+    for ((block, list) in newLists) {
+      println(block)
+      println(list.stringify())
+      println()
+    }
+    for ((value, register) in allocs) {
+      println("allocate $value to $register")
+    }
+    return newLists to allocs
+  }
+
   @Test
   fun `Instruction Selection On Simple Adds`() {
     val add = IntBinary(
@@ -41,39 +52,21 @@ class X64Tests {
   @Test
   fun `Instruction Selection On CFG`() {
     val cfg = prepareCFG(resource("addsAndMovs.c"), source)
-    val iselLists = X64Target.instructionSelection(cfg)
-    for (list in iselLists) {
+    val iLists = X64Target.instructionSelection(cfg)
+    for (list in iLists) {
       println(list.value.stringify())
     }
   }
 
   @Test
   fun `Register Allocation`() {
-    val cfg = prepareCFG(resource("interference.c"), source)
-    val (newLists, allocs) = X64Target.regAlloc(cfg, X64Target.instructionSelection(cfg))
-    for ((block, list) in newLists) {
-      println(block)
-      println(list.stringify())
-      println()
-    }
-    for ((value, register) in allocs) {
-      println("allocate $value to $register")
-    }
+    val (_, allocs) = regAlloc("interference.c")
     assertEquals(3, allocs.values.distinct().size)
   }
 
   @Test
   fun `Register Allocation With Register Pressure`() {
-    val cfg = prepareCFG(resource("highRegisterPressure.c"), source)
-    val (newLists, allocs) = X64Target.regAlloc(cfg, X64Target.instructionSelection(cfg))
-    for ((block, list) in newLists) {
-      println(block)
-      println(list.stringify())
-      println()
-    }
-    for ((value, register) in allocs) {
-      println("allocate $value to $register")
-    }
+    val (_, allocs) = regAlloc("highRegisterPressure.c")
     assertEquals(14, allocs.values.filter { it.valueClass != Memory }.distinct().size)
     val spilled = allocs.entries.filter { it.value.valueClass == Memory }
     assertEquals(1, spilled.size)
