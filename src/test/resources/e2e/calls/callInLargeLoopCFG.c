8d2ba9bda0d134a563321fa7a76a8c176f21880a
Stefan Silviu <sylviu44@gmail.com>
2020-10-17 12:07:59 +0300

    Fix test

--- src/test/resources/e2e/calls/callInLargeLoopCFG.c
+++ src/test/resources/e2e/calls/callInLargeLoopCFG.c
@@ -12,8 +12,8 @@ int main() {
     if (i == 4) x4 = thing(5, -6);
     if (i == 5) x5 = thing(6, -7);
     if (i == 6) x6 = thing(7, -8);
-    if (i == 7) x7 = thing(8, -8);
-    if (i == 8) x8 = thing(9, -9);
+    if (i == 7) x7 = thing(8, -9);
+    if (i == 8) x8 = thing(9, -10);
     if (i == 9) x9 = thing(10, 0);
   }
   return x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9; // 1
