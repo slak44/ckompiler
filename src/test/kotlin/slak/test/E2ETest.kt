03dfee38f5306fe5f79b9987211c47175822530f
Stefan Silviu <sylviu44@gmail.com>
2021-02-01 18:32:14 +0200

    Catch exit code signals for E2E tests

--- src/test/kotlin/slak/test/E2ETest.kt
+++ src/test/kotlin/slak/test/E2ETest.kt
@@ -4,6 +4,7 @@ import slak.ckompiler.ExitCodes
 import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 internal class CompileAndRunBuilder {
   var text: String? = null
@@ -22,6 +23,42 @@ internal fun RunResult.expect(exitCode: Int = 0, stdout: String = "", stderr: St
   assertEquals(stdout, this.stdout, "stdout is wrong;")
   assertEquals(stderr, this.stderr, "stderr is wrong;")
 }
+
+// kill -l gets this list
+// Not all of the signals make sense as a return code, but include them for completion
+private val signalMap = mapOf(
+    1 to "SIGHUP",
+    2 to "SIGINT",
+    3 to "SIGQUIT",
+    4 to "SIGILL",
+    5 to "SIGTRAP",
+    6 to "SIGABRT",
+    7 to "SIGBUS",
+    8 to "SIGFPE",
+    9 to "SIGKILL",
+    10 to "SIGUSR1",
+    11 to "SIGSEGV",
+    12 to "SIGUSR2",
+    13 to "SIGPIPE",
+    14 to "SIGALRM",
+    15 to "SIGTERM",
+    16 to "SIGSTKFLT",
+    17 to "SIGCHLD",
+    18 to "SIGCONT",
+    19 to "SIGSTOP",
+    20 to "SIGTSTP",
+    21 to "SIGTTIN",
+    22 to "SIGTTOU",
+    23 to "SIGURG",
+    24 to "SIGXCPU",
+    25 to "SIGXFSZ",
+    26 to "SIGVTALRM",
+    27 to "SIGPROF",
+    28 to "SIGWINCH",
+    29 to "SIGIO",
+    30 to "SIGPWR",
+    31 to "SIGSYS",
+)
 
 internal fun <T : Any> T.compileAndRun(block: CompileAndRunBuilder.() -> Unit): RunResult {
   val builder = CompileAndRunBuilder()
@@ -63,6 +100,14 @@ internal fun <T : Any> T.compileAndRun(block: CompileAndRunBuilder.() -> Unit): 
   val stdout = process.inputStream.bufferedReader().readText()
   val stderr = process.errorStream.bufferedReader().readText()
   val exitCode = process.waitFor()
+
+  // Signals are unix stuff
+  val os = System.getProperty("os.name")
+  val isUnix = os == "Linux" || os.startsWith("Mac OS")
+  if (isUnix && exitCode > 128) {
+    fail("Execution finished with large exit code $exitCode (name: ${signalMap[exitCode - 128]})")
+  }
+
   return RunResult(exitCode = exitCode, stdout = stdout, stderr = stderr)
 }
 
