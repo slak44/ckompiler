76dd0522d8db29c90c3397abbb75bd7d731bc3d2
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2021-12-03 21:30:39 +0200

    Remove default angular template

--- internals-explorer/src/app/app-routing.module.ts
+++ internals-explorer/src/app/app-routing.module.ts
@@ -7,4 +7,5 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- internals-explorer/src/app/app.module.ts
+++ internals-explorer/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,4 +16,5 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
