118f609ef0760f56750da642fa6611e3cdc3a183
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-02-17 20:55:00 +0200

    Add phase-in animation for algorithm

--- internals-explorer/src/app/phi-graph-view/components/insertion-algorithm/insertion-algorithm.component.ts
+++ internals-explorer/src/app/phi-graph-view/components/insertion-algorithm/insertion-algorithm.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { PhiInsertionStateService } from '../../services/phi-insertion-state.service';
+import { phaseInOut } from '@cki-utils/phase-in-out';
 
 @Component({
   selector: 'cki-insertion-algorithm',
   templateUrl: './insertion-algorithm.component.html',
   styleUrls: ['./insertion-algorithm.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [phaseInOut],
 })
 export class InsertionAlgorithmComponent {
   public readonly variableName$: Observable<string> = this.phiInsertionStateService.targetVariable$.pipe(
