cf58d6ecc2bea3ea2b1d5e0d15082689d30f776f
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-03-04 20:53:21 +0200

    Apply replace node contents hook to renaming

--- internals-explorer/src/app/renaming-graph-view/components/var-rename-view/var-rename-view.component.ts
+++ internals-explorer/src/app/renaming-graph-view/components/var-rename-view/var-rename-view.component.ts
@@ -5,22 +5,29 @@ import { GraphViewHook } from '@cki-graph-view/models/graph-view-hook.model';
 import { RenamingStateService } from '../../services/renaming-state.service';
 import { slak } from '@ckompiler/ckompiler';
 import JSCompileResult = slak.ckompiler.JSCompileResult;
+import { ReplaceNodeContentsHook } from '@cki-graph-view/graph-view-hooks/replace-node-contents';
 
 @Component({
   selector: 'cki-var-rename-view',
   templateUrl: './var-rename-view.component.html',
   styleUrls: ['./var-rename-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [RenamingIrFragmentComponent.provider],
+  providers: [
+    RenamingIrFragmentComponent.provider,
+    ReplaceNodeContentsHook,
+  ],
 })
 export class VarRenameViewComponent {
   public readonly printingType$: Observable<string> = of('IR_TO_STRING');
 
   public readonly compileResult$: Observable<JSCompileResult> = this.renamingStateService.compileResult$;
 
-  public readonly hooks: GraphViewHook[] = [];
+  public readonly hooks: GraphViewHook[] = [
+    this.replaceNodeContentsHook,
+  ];
 
   constructor(
+    private replaceNodeContentsHook: ReplaceNodeContentsHook,
     private renamingStateService: RenamingStateService,
   ) {
   }
