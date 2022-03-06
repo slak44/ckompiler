f922b9dd00f51b1746e0455567599bb7926b902f
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-03-06 12:18:30 +0200

    Add basic test for external exporting function

---
+++ src/test/kotlin/slak/test/analysis/ExportTests.kt
@@ -0,0 +1,17 @@
+package slak.test.analysis
+
+import org.junit.jupiter.api.Test
+import slak.ckompiler.analysis.external.generateRenameSteps
+import slak.test.prepareCFG
+import slak.test.resource
+import slak.test.source
+
+class ExportTests {
+  @Test
+  fun `Generate Renaming States Works`() {
+    val cfg = prepareCFG(resource("ssa/phiTest.c"), source)
+    for (variable in cfg.exprDefinitions.keys) {
+      generateRenameSteps(cfg, variable)
+    }
+  }
+}
