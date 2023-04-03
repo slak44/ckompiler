a74c93eded2853c846c5e1773d0b6ce5dce986aa
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-04-03 20:32:05 +0300

    Relax allocation correctness checker

--- src/main/kotlin/slak/ckompiler/backend/WalkGraphAllocs.kt
+++ src/main/kotlin/slak/ckompiler/backend/WalkGraphAllocs.kt
@@ -10,7 +10,11 @@ enum class ViolationType {
   /** A hard violation is a definite error in the allocation. */
   HARD,
 
-  /** A soft violation might have been caused by two-address code, and might still be a correct allocation. */
+  /**
+   * A soft violation is an instruction that both uses and defines the same register.
+   * This is common in two-address code, but it happens in three-address code when the result is equal to an operand.
+   * It is usually not an error for this to happen.
+   */
   SOFT
 }
 
@@ -32,22 +36,23 @@ inline fun AllocationResult.walkGraphAllocs(
     val alive = mutableListOf<MachineRegister>()
     alive += graph.liveness.liveInsOf(blockId).map { allocations.getValue(it) }
     val usedByPhi = graph[blockId].phiUses.filter { !it.isUndefined }
-    alive -= usedByPhi.map { allocations.getValue(it) }
+    alive -= usedByPhi.map { allocations.getValue(it) }.toSet()
     for ((index, mi) in graph[blockId].withIndex()) {
-      val regsDefinedHere = mi.defs.intersect(allocated)
+      val regsDefinedHere = mi.defs.intersect(allocated.toSet())
           .filter { graph.liveness.isUsed(it as AllocatableValue) && !it.isUndefined }
           .map { allocations.getValue(it) } +
           mi.defs.filterIsInstance<PhysicalRegister>().map { it.reg }
       val regsDyingHere = graph.dyingAt(InstrLabel(blockId, index), mi)
-          .intersect(allocated)
+          .intersect(allocated.toSet())
           .filter { !it.isUndefined }
           .map { allocations.getValue(it) } +
           mi.uses.filterIsInstance<PhysicalRegister>().map { it.reg }
-      val useDefRegs = mi
-          .filterOperands(listOf(VariableUse.DEF_USE))
+      val defUseValues = mi.filterOperands(listOf(VariableUse.DEF_USE)) + mi.defs.intersect(mi.uses.toSet())
+      val useDefRegs = defUseValues
           .filterIsInstance<AllocatableValue>()
           .map { allocations.getValue(it) }
-      alive -= regsDyingHere
+          .toSet()
+      alive -= regsDyingHere.toSet()
       for (definedHere in regsDefinedHere) {
         if (definedHere in alive) {
           val violationType = if (definedHere !in useDefRegs) ViolationType.HARD else ViolationType.SOFT
