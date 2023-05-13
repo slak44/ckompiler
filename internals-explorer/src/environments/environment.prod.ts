47f8a6c5fc3e2544190365da7040309317b5f23b
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-05-13 18:34:30 +0300

    Add production IP

--- internals-explorer/src/environments/environment.prod.ts
+++ internals-explorer/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnvironmentType } from './environment-type';
 
 export const environment: EnvironmentType = {
   production: true,
-  baseUrl: 'https://TBD/api',
+  baseUrl: 'http://128.140.49.235/api',
   oauth: {
     domain: 'ckompiler-internals-explorer.eu.auth0.com',
     clientId: 'VX6YGntpyUipXIFXHff4clmJe2geq8rb',
