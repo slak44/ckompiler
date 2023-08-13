2acc2b24ba1297aff471d038467325c40eac119b
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-08-13 20:19:49 +0300

    Fix deprecated import

--- internals-explorer/src/app/app.module.ts
+++ internals-explorer/src/app/app.module.ts
@@ -55,7 +55,7 @@ import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig, MatSnackBarModule } f
         languages: {
           c: () => import('highlight.js/lib/languages/c'),
           x86asm: () => import('highlight.js/lib/languages/x86asm'),
-          mipsasm: () => import('highlight.js/lib/languages/mipsasm.js'),
+          mipsasm: () => import('highlight.js/lib/languages/mipsasm'),
         },
       },
     },
