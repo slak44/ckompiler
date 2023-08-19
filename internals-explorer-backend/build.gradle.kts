8340dd8fd10d8dd92ba8ac2cfa14f7685f52023f
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-08-19 09:48:34 +0300

    Update kotlin version for spring project

--- internals-explorer-backend/build.gradle.kts
+++ internals-explorer-backend/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
   id("org.springframework.boot") version "3.0.5"
   id("io.spring.dependency-management") version "1.1.0"
   id("org.liquibase.gradle") version "2.0.4"
-  kotlin("jvm") version "1.7.22"
-  kotlin("plugin.spring") version "1.7.22"
-  kotlin("plugin.jpa") version "1.7.22"
+  kotlin("jvm") version "1.9.0"
+  kotlin("plugin.spring") version "1.9.0"
+  kotlin("plugin.jpa") version "1.9.0"
 }
 
 group = "slak"
