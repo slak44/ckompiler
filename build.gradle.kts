92d7e2ce6adbc76e2580841083567b6b663546ca
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-09-30 13:42:56 +0300

    Fix dependency name

--- build.gradle.kts
+++ build.gradle.kts
@@ -190,7 +190,7 @@ kotlin {
         implementation(kotlin("stdlib-jdk8"))
         implementation("com.github.ajalt:mordant:1.2.0")
         implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-        implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.20.0")
+        implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
       }
     }
 
