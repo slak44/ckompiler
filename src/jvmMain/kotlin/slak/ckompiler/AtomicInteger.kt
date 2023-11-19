7a57e8ece3fb51ac31194c617626baa9256f5ac8
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2023-11-19 11:53:18 +0200

    Simplify JVM AtomicInteger actual class

--- src/jvmMain/kotlin/slak/ckompiler/AtomicInteger.kt
+++ src/jvmMain/kotlin/slak/ckompiler/AtomicInteger.kt
@@ -1,13 +1,3 @@
 package slak.ckompiler
 
-actual class AtomicInteger {
-  private val jvmAtomicInteger = java.util.concurrent.atomic.AtomicInteger()
-
-  actual fun getAndIncrement(): Int = jvmAtomicInteger.getAndIncrement()
-
-  actual fun get(): Int = jvmAtomicInteger.get()
-
-  actual fun set(value: Int) {
-    jvmAtomicInteger.set(value)
-  }
-}
+actual typealias AtomicInteger = java.util.concurrent.atomic.AtomicInteger
