24dda2d1631758eabb461116558c63a5a126506d
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2024-02-03 12:09:34 +0200

    Auto-logout if refresh token is expired

--- internals-explorer/src/app/app.component.ts
+++ internals-explorer/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { SubscriptionDestroy } from '@cki-utils/subscription-destroy';
+import { takeUntil } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'cki-root',
@@ -13,12 +17,29 @@ import { RouterOutlet } from '@angular/router';
     RouterOutlet,
   ],
 })
-export class AppComponent {
+export class AppComponent extends SubscriptionDestroy {
   constructor(
+    private readonly authService: AuthService,
     private readonly location: Location,
   ) {
+    super();
+
     window.__hpcc_wasmFolder = this.location.prepareExternalUrl('/assets/graphviz-wasm');
     const workerPath = this.location.prepareExternalUrl('/assets/graphviz-wasm/index.min.js');
     document.head.insertAdjacentHTML('beforeend', `<script src="${workerPath}" type="javascript/worker"></script>`);
+
+    this.authService.error$.pipe(
+      takeUntil(this.destroy$),
+    ).subscribe(error => {
+      console.error(error);
+
+      if (error.message.includes('Unknown or invalid refresh token')) {
+        this.authService.logout({
+          logoutParams: {
+            returnTo: environment.rootUrl,
+          },
+        });
+      }
+    });
   }
 }
